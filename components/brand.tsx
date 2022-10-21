0dad0d5d721a5b59be2c053ef613aee7813eca6f
Franco Aguilera <flaalf@gmail.com>
2022-10-21 17:01:44 -0300

    This bug has driven lots of coders completely mad. You won't believe how it ended up being fixed

---
+++ components/brand.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Branding = () => {
+  return (
+    <>
+      <h3>Author: Franco Aguilera</h3>
+      <h3>
+        <a href="https://github.com/FrancoAguilera" target="_blank">
+          [ :_ Github ]
+        </a>
+      </h3>
+      <h3>
+        <a href="https://twitter.com/Ahsen_Juan" target="_blank">
+          [ @Twitter ]
+        </a>
+      </h3>
+      <h3>
+        <a href="https://twitter.com/Ahsen_Juan" target="_blank">
+          [ StackOverflow ]
+        </a>
+      </h3>
+    </>
+  );
+};
