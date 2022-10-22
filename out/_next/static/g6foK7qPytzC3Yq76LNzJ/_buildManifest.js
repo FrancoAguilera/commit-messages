510ffb3c45112a7a285ffc6b646b8b5a3c157326
Franco Aguilera <flaalf@gmail.com>
2022-10-22 00:57:07 -0300

    set nojekyll

---
+++ out/_next/static/g6foK7qPytzC3Yq76LNzJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/c16184b3-863cf5a1089b5d02.js","static/chunks/603-ff98fd67907a9766.js","static/chunks/pages/index-f26ccdf60581eabb.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
