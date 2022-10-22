51a3cea71550ced4d53fa2241949e327fde98a1d
Franco Aguilera <flaalf@gmail.com>
2022-10-22 01:01:46 -0300

    set nojekyll

--- components/brand.tsx
+++ components/brand.tsx
@@ -15,7 +15,7 @@ export const Branding = () => {
         </a>
       </h3>
       <h3>
-        <a href="https://twitter.com/Ahsen_Juan" target="_blank" rel="noreferrer">
+        <a href="https://stackoverflow.com/users/2743774/franco-aguilera" target="_blank" rel="noreferrer">
           [ StackOverflow ]
         </a>
       </h3>
